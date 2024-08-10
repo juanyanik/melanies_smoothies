d32ed82e2f10f2b8f45f6289990a52b11c9a9bb4
juanyanik <100418934+juanyanik@users.noreply.github.com>
2024-08-10 15:01:22 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -9,7 +8,8 @@ st.title("Customize your smoothie :balloon:")
 name_order = st.text_input("Name on smoothie:")
 st.write('The name on your smoothie will be:', name_order)
 
-session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME')).collect()
 
 # Convert the collected data to a list for the multiselect
